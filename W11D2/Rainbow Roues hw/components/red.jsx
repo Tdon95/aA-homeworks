2b0915d0ea968edd377c875846c859eaeecccf14
Torin Donahue <torin.donahue95@gmail.com>
2020-07-21 08:43:09 -0400

    Rainbow Routes

---
+++ W11D2/Rainbow Roues hw/components/red.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        {/* Links here */}
+
+        {/* Routes here */}
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
+      </div>
+    );
+  }
+};
+
+export default Red;
